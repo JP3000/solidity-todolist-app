41aec3f30411941b5b5493de6b65aec0b97433f8
JP3000 <2691967523@qq.com>
2025-08-16 21:23:50 +0800

    contract is deployed

---
+++ backend/contracts/TaskContract.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+// task:{ id: 0, taskText: 'clean', isDeleted: false }, 
+// DEPLOYED Contract Address
+// ABI 
+
+
+contract TaskContract {
+    event AddTask(address recipient, uint taskId);
+    event DeleteTask(uint taskId, bool isDeleted);
+
+    struct Task {
+        uint id;
+        string taskText;
+        bool isDeleted;
+    }
+
+    Task[] private tasks;
+    mapping(uint256 => address) taskToOwner;
+    // {0: '0x123124124...'}
+    // {1: '0xabc124124...'}
+
+    function addTask(string memory taskText, bool isDeleted) external {
+        uint taskId = tasks.length;
+        tasks.push(Task(taskId, taskText, isDeleted));
+        taskToOwner[taskId] = msg.sender;
+        emit AddTask(msg.sender, taskId);
+    }
+
+    // get tasks that are mine and not deleted
+    function getMyTasks() external view returns (Task[] memory) {
+        Task[] memory temporary = new Task[](tasks.length);
+        uint counter = 0;
+
+        // temporary [{taskText:"hello", isDeleted:false}, empty]
+        for (uint i=0; i < tasks.length; i++) {
+        if (taskToOwner[i] == msg.sender && tasks[i].isDeleted == false) {
+            temporary[counter] = tasks[i];
+            counter++;
+        }
+        }
+
+        // Resize the temporary array to fit the actual number of tasks
+        Task[] memory result = new Task[](counter);
+        for (uint i = 0; i < counter; i++) {
+        result[i] = temporary[i];
+        }
+        return result;
+    }
+
+    function deleteTask(uint taskId, bool isDeleted) external {
+        if(taskToOwner[taskId] == msg.sender){ 
+        tasks[taskId].isDeleted = isDeleted;
+        emit DeleteTask(taskId, isDeleted);
+        }
+    }
+
+}
